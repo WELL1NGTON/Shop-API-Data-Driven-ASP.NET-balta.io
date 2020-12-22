dd8ac23fdbb608225a7dd6d5ae1b3125e7bacf42
WELL1NGTON <wellingtonmassola@gmail.com>
2020-12-22 16:41:14 -0300

    27. Autenticação e Autorização

---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop
+{
+    public class Settings
+    {
+        public static string Secret = "n3KKVCMar/C7PYktUZLFJn7gomZdP2AOD36Oxmi16t47JVMU1WM9rBIboyYrNYh/kFa2JYJyQ8Is4zThpz9NQg==";
+    }
+}
