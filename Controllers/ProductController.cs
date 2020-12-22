5ffca98e143f443d75751d4e4a8e891684484ecf
WELL1NGTON <wellingtonmassola@gmail.com>
2020-12-22 15:48:51 -0300

    24. Finalizando Controller de Produtos

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -55,5 +55,20 @@ namespace shop.Controllers
 
             return Ok(products);
         }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post(
+            [FromServices] DataContext context,
+            [FromBody] Product model
+        )
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            context.Products.Add(model);
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
     }
 }
